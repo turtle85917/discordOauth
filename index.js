fb3e60fe286acca2911eb0e297938e84ed03526e
turtle85917 <siu85917@gmail.com>
2022-02-27 15:55:03 +0900

    Edit

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ app.get('/discordOauth/', function(req, res) {
   res.send('Hello World!');
 });
 
-let port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
-});
+app.listen(3000);
